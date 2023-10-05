54b16005f25426b1cdd87785e39d1ef90c169476
Richard Lane <richard.lane@bristol.ac.uk>
2023-10-05 15:27:05 +0100

    skel for the lat-long thing

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # latam_networks
 Visualising conference networks in Latin America
 
-# How to run
+## What this script does
+I've taken the Word file of Americanists 1939 conference attendees and put them in a spreadsheet - this lives on the JGI SharePoint.
+This script reads that spreadsheet and does the following:
+ - Looks at each attendee, and tries to find the location of their origin city
+    - If this fails, it tries to find the location of the country's capital city
+    - This may make the visualisation a little misleading, but one can indicate on the map whether this has happened if it is a factor
+ - Draws a map of the world
+ - Draws arrows from each origin point to Lima, where the conference was held. The width of this arrow is proportional to the number of attendees
+
+### How to run
  - Install the requirements
  - Get the required data
-    - The script will do most of this for you, but you'll need to put the spreadsheet `americanists_attendees.xlsx` (this can be found on the JGI OneDrive) in the `data/` directory
+    - The script will do most of this for you, but you'll need to put the spreadsheet `americanists_attendees.xlsx` (this can be found on the JGI SharePoint) in the `data/` directory
  - Run `prototype.py`
 
-# Data Sources
+### Data Sources
 City locations lookup table from `https://simplemaps.com/data/world-cities`
--- prototype.py
+++ prototype.py
@@ -14,6 +14,32 @@ def read_data() -> pd.DataFrame:
     return pd.read_excel("data/americanists_attendees.xlsx", sheet_name="Tabelle1")
 
 
+def get_city_locations() -> pd.DataFrame:
+    """
+    Download + unzip the table of city locations if required, then read + return it as a dataframe
+
+    """
+
+
+def get_lat_long(data: pd.DataFrame) -> tuple:
+    """
+    Get the latitude and longitude for each entry in the data
+
+    This will first try to get the location of each city; if this isn't possible, it will try to get the location of the country
+
+    If this also fails it will enter NaN
+
+    """
+    # Download the table of city locations if not already downloaded
+    get_city_locations()
+
+    # For each city look up latitude and longitude
+
+    # If it isn't found, try finding the country's capital
+
+    # If this still isn't found, enter NaN
+
+
 def main():
     """
     Read in and clean the data and make a basic visualisation suitable as a proof-of-concept
@@ -24,7 +50,14 @@ def main():
     # Clean the data
     data["City"] = data["City"].str.strip()
 
-    # See which cities we have
+    # Get the latitude/longitude for each entry
+    latitudes, longitudes = get_lat_long(data)
+    data["lat"] = latitudes
+    data["long"] = longitudes
+
+    # Get + plot a shapefile for the world
+
+    # On the shapefile, draw an arrow from the origin point to Lima (where the conference took place)
 
 
 if __name__ == "__main__":
